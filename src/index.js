e101f4f3d64fa5414aba41aafa3a90e136423f6c
dwayne314 <dwayne.manlove@gmail.com>
2020-04-14 18:10:12 -0400

    Installed create-react-app and created the directory layout

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './pages/Main/Main';
+
+import './index.css';
+
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('root')
+);
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from '../../components/App/App';
+
+
+const Main = () => {
+    return (
+        <div className="main-container">
+            <App />
+        </div>
+    )
+}
+
+export default Main;
