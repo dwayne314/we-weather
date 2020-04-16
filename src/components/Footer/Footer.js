5ea17d292c3b4cff2bb58cafaf4ca75ae57acfe8
dwayne314 <dwayne.manlove@gmail.com>
2020-04-16 00:45:18 -0400

    Added the general layout for the footer.
    Included footer variables in the root css to protect against screeen re-sizing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,48 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <div className="footer-container"></div>
+        <div className="footer-container">
+        	<div className="footer-section">
+        		<div className="footer-section-text">
+        			Friday
+        		</div>
+        		<div className="footer-section-temp">
+        			90°
+        		</div>
+        	</div>
+        	<div className="footer-section">
+        		<div className="footer-section-text">
+        			Saturday
+        		</div>
+        		<div className="footer-section-temp">
+        			90°
+        		</div>
+        	</div>
+        	<div className="footer-section">
+        		<div className="footer-section-text">
+        			Sunday
+        		</div>
+        		<div className="footer-section-temp">
+        			90°
+        		</div>
+        	</div>
+        	<div className="footer-section">
+        		<div className="footer-section-text">
+        			Monday
+        		</div>
+        		<div className="footer-section-temp">
+        			90°
+        		</div>
+        	</div>
+        	<div className="footer-section">
+        		<div className="footer-section-text">
+        			Tuesday
+        		</div>
+        		<div className="footer-section-temp">
+        			90°
+        		</div>
+        	</div>
+        </div>
     );
 };
 
